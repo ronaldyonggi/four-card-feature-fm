716b516f23b9b5bbf6b84ebbb7186b4088dbf56f
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-10-05 19:18:55 +0800

    containerize text part, add style indexing for card

--- src/components/FeatureCard/FeatureCard.tsx
+++ src/components/FeatureCard/FeatureCard.tsx
@@ -3,16 +3,19 @@ import styles from './FeatureCard.module.css';
 
 interface FeatureCardProps {
   feature: IFeature;
+  className: string;
 }
 
-export default function FeatureCard({ feature }: FeatureCardProps) {
+export default function FeatureCard({ feature, className }: FeatureCardProps) {
   const cardBorderTopColor = {
     borderTop: `3.5px solid var(--color-primary-${feature.color})`,
   };
   return (
-    <div className={styles.container} style={cardBorderTopColor}>
-      <h3 className={styles.title}>{feature.title}</h3>
-      <p className={styles.description}>{feature.description}</p>
+    <div className={`${styles.container} ${className}`} style={cardBorderTopColor}>
+      <div className={styles.textContainer}>
+        <h3 className={styles.title}>{feature.title}</h3>
+        <p className={styles.description}>{feature.description}</p>
+      </div>
       <div className={styles.imageContainer}>
         <img src={feature.icon} alt='Icon feature' />
       </div>
