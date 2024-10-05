244fde5b0a4b15eb73684af6490a06c7929da8c8
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-10-05 14:21:06 +0800

    define main app

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,18 @@
+import './styles/reset.css'
+import './styles/globals.css'
 import { createRoot } from 'react-dom/client'
+import Header from './components/Header/Header'
+import FeatureList from './components/FeatureList/FeatureList'
+
+
+export function App() {
+  return (
+    <main>
+      <Header />
+      <FeatureList />
+    </main>
+  )
+}
 
 createRoot(document.getElementById('root')!).render(
     <App />
